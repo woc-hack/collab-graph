bd7fbd0749d4b642d9b4a2f0c975e3d9477b51eb
elena-lyulina <elena.lyulina@gmail.com>
2020-12-05 01:54:12 +0100

    Add some web examples

---
+++ frontend/web/network-description.js
@@ -0,0 +1,5 @@
+const div = document.getElementById('authors-static');
+div.addEventListener('mouseenter', function() {
+    const desc = document.getElementById('projects-description');
+    desc.style.display = 'non'
+}, false);
